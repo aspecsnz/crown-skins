f77a35aff481854c05702888c3c8b4b88150893e
aspecsnz <72107888+aspecsnz@users.noreply.github.com>
2020-11-19 20:38:36 +1300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    Tebex: {
+        // A server's secret so we can query your store updating transactions.
+
+        Secret: "",
+        
+        // Hostname of your Tebex store
+        
+        Hostname: "https://plugin.tebex.io",
+        
+        // Where you want users to redirect to once the transactions has successfully processed.
+
+        CompleteRedirect: "",
+
+        // Where you want users to redirect to if something went wrong (cancelled, expired)
+
+        ErrorRedirect: ""
+    },
+    Skins: {
+        // Hostname of your Skins instance
+        
+        Hostname: "https://crownskins.com",
+
+        // Username and Password of your Skins instance
+        
+        Auth: {
+            Username: "",
+            Password: ""
+        },
+        
+        // Account Payout Skins will be sent to once the transaction has successfully been processed.
+
+        PayoutTradeURL: ""
+    },
+    Webserver: {
+        // Port this intergration app listens to
+        
+        Port: 3040,
+
+        // Hostname of this intergration app (does not need to be a domain!)
+
+        Hostname: ""
+    }
+};
---
+++ index.js
@@ -0,0 +1,66 @@
+const config = require('./config');
+
+const Tebex = require('./lib/Tebex');
+const Skins = require('./lib/Skins');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = require('http').createServer(app);
+
+app.get('/', async (req, res) =>
+{
+    if (!req.query || !req.query.reference)
+    {
+        return res.sendStatus(400);
+    }
+
+    let { reference } = req.query;
+    let { can_proceed, payment } = await Tebex.payments.canProceedToPayment(reference);
+
+    if (!can_proceed)
+    {
+        return res.redirect(config.Tebex.ErrorRedirect);
+    }
+
+    let skins_data = await Skins.payments.ensure(reference, payment);
+        
+    return res.redirect(skins_data.approval_url);
+});
+
+app.get('/return', async (req, res) =>
+{
+    if (!req.query || !req.query.transaction_id)
+    {
+        return res.sendStatus(400);
+    }
+
+    let { transaction_id } = req.query;
+
+    let payment = await Skins.payments.fetch(transaction_id);  
+
+    if (payment.status === "Accepted")
+    {
+        await Tebex.payments.markPaymentAsPaid(req.query.transaction_id);
+    }
+    else
+    {
+        return res.redirect(config.Tebex.ErrorRedirect);
+    }
+
+    if (!req.query.auto)
+    {
+        return res.redirect(config.Tebex.CompleteRedirect.replace("{REFERENCE}", transaction_id));
+    }
+        
+    return res.sendStatus(200);
+})
+
+app.get('/cancel', (req, res) => res.redirect(config.Tebex.ErrorRedirect));
+
+server.listen(config.Webserver.Port, () => console.log(`[Express] Listening on port ${config.Webserver.Port}.`));
